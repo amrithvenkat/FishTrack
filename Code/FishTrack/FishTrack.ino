4c43a56e8e27282f01f37bd6dec0f430b6b0d0e5
Jeevaraam Kumar <jeevaraam@live.in>
2021-06-04 14:31:14 +0530

    Folders restructuring

---
+++ Code/FishTrack/FishTrack.ino
@@ -0,0 +1,31 @@
+#include "WORK.H"
+
+void setup() {
+  // put your setup code here, to run once:
+prepare();
+}
+
+void loop() {
+  bool newData=false;
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    logs=SD.open("NMEAF.txt",FILE_WRITE);
+    while (Serial3.available())
+    {
+      char c = Serial3.read();
+      logs.print(c);
+      if (gps.encode(c)) 
+        newData = true;
+    }
+    logs.close();
+  }
+  if(newData)
+  {
+   String data= datanew();
+   writelog(data);
+   test();
+  }
+ displayupdate();
+ bordercross();
+}
+
